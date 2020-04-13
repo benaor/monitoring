541e4bf592e8bcbc5553920f67b8a92444c3d0a1
benaor <benjamingirard25@gmail.com>
2020-04-13 15:12:10 +0200

    Display all website on HomePage

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
 ];
--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,30 @@
 
 namespace App\Controller;
 
+use App\Repository\WebsiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
-     * @Route("/home", name="home")
+     * @Route("/", name="home")
      */
-    public function index()
+    public function index(WebsiteRepository $websiteRepo)
     {
+        $websites = $websiteRepo->findAll();
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'websites' => $websites,
+        ]);
+    }
+
+    /**
+     * @Route("/website/{id}", name="website_show")
+     */
+    public function show()
+    {
+        return $this->render('home/show.html.twig', [
+            
         ]);
     }
 }
