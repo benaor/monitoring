da6036bbbcc9069d6db46ff440bad65154c9ac05
benaor <benjamingirard25@gmail.com>
2020-04-14 15:53:21 +0200

    create function Analyse

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Status;
 use App\Entity\Website;
 use App\Repository\WebsiteRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
@@ -21,12 +23,41 @@ class HomeController extends AbstractController
     }
 
     /**
+     * @Route("/website/analyse", name="analyse")
+     */
+    public function analyse(WebsiteRepository $websiteRepo, EntityManagerInterface $manager)
+    {
+        // Recuperate All websites
+        $websites = $websiteRepo->findAll();
+
+        // recuperate status of actualy this one
+        foreach ($websites as $key => $site) {
+            $url = $site->getUrl();
+            $handle = curl_init($url);
+            curl_setopt($handle, CURLOPT_RETURNTRANSFER, TRUE);
+            $response = curl_exec($handle);
+            $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+            curl_close($handle);
+            
+            $status = new Status();
+            $status->setCode($code)
+                    ->setReportedAt(new \DateTime())
+                    ->setWebsite($site);
+            $manager->persist($status);
+        }
+        $manager->flush();
+        // create new entity status et register in database
+        // redirect to route ('home')
+        return $this->redirectToRoute('home');
+    }
+
+    /**
      * @Route("/website/{id}", name="website_show")
      */
     public function show(Website $website)
     {
         return $this->render('home/show.html.twig', [
-        'website' => $website
+            'website' => $website
         ]);
     }
 }
