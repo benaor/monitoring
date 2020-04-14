058420b0e159cc3660071f8be9f950c22465e1a0
benaor <benjamingirard25@gmail.com>
2020-04-14 22:45:43 +0200

    create Admin Dashboard and display all website on this

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,18 +2,20 @@
 
 namespace App\Controller;
 
+use App\Repository\WebsiteRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
+     * @Route("/admin", name="admin_dashboard")
      */
-    public function index()
+    public function index(WebsiteRepository $websiteRepo)
     {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
+        $website = $websiteRepo->findAll();
+        return $this->render('admin/admin.html.twig', [
+            'websites'=> $website
         ]);
     }
 }
