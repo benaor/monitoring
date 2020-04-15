6493ddfbd0a3748f216464231c8223ca2a8ed296
benaor <benjamingirard25@gmail.com>
2020-04-15 23:21:58 +0200

    Page and function new website

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,8 +5,10 @@ namespace App\Controller;
 use App\Entity\Website;
 use App\Form\WebsiteType;
 use App\Repository\WebsiteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class AdminController extends AbstractController
 {
@@ -24,10 +26,20 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin/new", name="admin_new")
      */
-    public function newWebsite()
+    public function newWebsite(Request $request, EntityManagerInterface $manager)
     {
         $website = new Website();
         $form = $this->createForm(WebsiteType::class, $website);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($website);
+            $manager->flush();
+            $this->addFlash('success', 'Vous avez ajouté un nouveau site');
+            return $this->redirectToRoute('admin_dashboard');
+        }
+
         return $this->render('admin/new.html.twig', [
             'form' => $form->createView()
         ]);
--- src/Entity/Website.php
+++ src/Entity/Website.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\WebsiteRepository")
@@ -24,6 +25,13 @@ class Website
     private $url;
 
     /**
+     * @Assert\Length(
+     *      min = 4,
+     *      max = 50,
+     *      minMessage = "Vous devez inscrire au moins {{ limit }} caracteres",
+     *      maxMessage = "Vous devez inscrire au maximum {{ limit }} caracteres",
+     *)
+     *      allowEmptyString = false
      * @ORM\Column(type="string", length=255)
      */
     private $name;
